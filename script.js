41782d203e7c6813ef678e3dbaea9318f49f074b
Bilgot <vilgot.marken@skola.taby.se>
2024-04-24 15:44:56 +0200

    jag fixar skiten

--- script.js
+++ script.js
@@ -8,10 +8,22 @@ function cardColor(){
     return colorList[Math.floor(Math.random * 4)]
 }
 
+function compareCard(yourCard, topCard){
+    if(yourCard.number === topCard.number || yourCard.color === topCard.color){
+        placeCard()
+    }  else{
+        //jadu
+    }
+}
+
 class Card{
     constructor(){
         this.number = randomCardNumber();
-        this.color = cardColor();
+        if(this.number === 14 || this.number === 15){
+            this.color = black;
+        } else {
+            this.color = cardColor()
+        }
     }
 }
 
