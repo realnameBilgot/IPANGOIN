c138c1e33ac69e78f6addad2f89448e1983f3b35
Bilgot <vilgot.marken@skola.taby.se>
2024-05-11 16:10:14 +0200

    Fixat bilderna

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
+const num0 = document.getElementById("num0")
+const num1 = document.getElementById("num1")
+const num2 = document.getElementById("num2")
+const num3 = document.getElementById("num3")
+const num4 = document.getElementById("num4")
+const num5 = document.getElementById("num5")
+const num6 = document.getElementById("num6")
+const num7 = document.getElementById("num7")
+const num8 = document.getElementById("num8")
+const num9 = document.getElementById("num9")
+const draw2 = document.getElementById("draw2")
+const stopNext = document.getElementById("stopNext")
+const reverse = document.getElementById("reverse")
+const wildDraw = document.getElementById("wildDraw")
+const wild = document.getElementById("wild")
+const cardBack = document.getElementById("cardBack")
+const blueBase = document.getElementById("blueBase")
+const greenBase = document.getElementById("greenBase")
+const redBase = document.getElementById("redBase")
+const yellowBase = document.getElementById("yellowBase")
+const logo = document.getElementById("logo")
 
 let colorList = ['red', 'blue', 'yellow', 'green'];
 
